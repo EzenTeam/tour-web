722bd8b5c49c6323966bfdf4c6e1ef2dc8149cb9
이재이 <loveleej87@gmail.com>
2022-05-25 15:29:28 +0900

    .
    .

---
+++ src/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+/**
+ * 전역 스타일 시트를 정의한 객체
+ * @type {GlobalStyleComponent<{}, DefaultTheme>}
+ */
+const GlobalStyles = createGlobalStyle`
+    * {font-family: ‘SUIT’, sans-serif;}
+
+    body {
+        margin: 0;
+        padding: 0;
+    }
+`;
+
+export default GlobalStyles;

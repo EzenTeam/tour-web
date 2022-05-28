a7def1e0d5e104fa6cf3252cb55ddcdc6c32a486
NaraeGu <nrggrnngg@gmail.com>
2022-05-28 20:05:29 +0900

    PetTravelStyles.js 수정

--- src/components/PetTravelStyles.js
+++ src/components/PetTravelStyles.js
@@ -14,36 +14,6 @@ const PetTravelStyles = styled.div`
         margin-bottom: 10px;
     }
 
-    .data-table {
-        width: 100%;
-
-        .list-title {
-            background: teal;
-            color: #fff;
-
-            tr {
-                th {
-                    padding: 10px 0;
-                }
-            }
-        }
-
-        .list-item {
-            tr {
-                td {
-                    text-align: center;
-                    padding: 10px 0;
-                    border-bottom: 1px solid #eee;
-                    border-right: 1px solid #eee;
-
-                    &:last-child {
-                        border-right: 0;
-                    }
-                }
-            }
-        }
-    }
-
     /* 라이브러리 css 변경사항 */
     .MuiFormLabel-root.Mui-focused {
         color: teal;
